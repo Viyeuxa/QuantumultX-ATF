b2aebdbdddd8a3d04b1abda71aaa4caa73c86c9e
Nguyễn Thế Việt <55195268+Viyeuxa@users.noreply.github.com>
2024-05-15 11:08:45 +0700

    Static APP_ID

---
+++ APP_ID_Static/Auto_join_TF.js
@@ -0,0 +1,77 @@
+//*********************************
+// */10 * * * * * https://raw.githubusercontent.com/Viyeuxa/QuantumultX-ATF/main/APP_ID_Static/Auto_join_TF.js, tag=TestFlight自动加入, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/testflight.png, enabled=true
+//*********************************
+
+!(async () => {
+  const githubUrl = "https://raw.githubusercontent.com/Viyeuxa/QuantumultX-ATF/main/APP_ID_Static/APP_ID_List.txt";
+  let ids = await fetch(githubUrl)
+    .then(response => response.text())
+    .then(text => text.split('\n').filter(id => id.trim() !== ''));
+    if (ids.length === 0) {
+      $notify("Danh sách APP_ID là rỗng", "Cần thêm APP_ID mới", "");
+      $done();
+    } else {
+      try {
+        for await (const ID of ids) {
+          await autoPost(ID);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    $done();
+})();
+
+function autoPost(ID) {
+  let Key = $prefs.valueForKey("key");
+  let testurl = "https://testflight.apple.com/v3/accounts/" + Key + "/ru/";
+  let header = {
+    "X-Session-Id": `${$prefs.valueForKey("session_id")}`,
+    "X-Session-Digest": `${$prefs.valueForKey("session_digest")}`,
+    "X-Request-Id": `${$prefs.valueForKey("request_id")}`,
+  };
+  return new Promise(function (resolve) {
+    $task.fetch({ url: testurl + ID, method: "GET", headers: header }).then(
+      (resp) => {
+        const { body: data } = resp;
+        if (resp.status == 404) {
+/*           ids = $prefs.valueForKey("APP_ID").split(",");
+          ids = ids.filter((ids) => ids !== ID);
+          $prefs.setValueForKey(ids.toString(), "APP_ID"); */
+          console.log(ID + " " + "không tồn tại hãy xoá APP_ID đó");
+          $notify(ID, "TF không tồn tại", "hãy xoá APP_ID đó");
+          resolve();
+        } else {
+          let jsonData = JSON.parse(data);
+          if (jsonData.data == null) {
+            console.log(ID + " " + jsonData.messages[0].message);
+            resolve();
+          } else if (jsonData.data.status == "FULL") {
+            console.log(ID + " " + jsonData.data.message);
+            resolve();
+          } else {
+            $task.fetch({ url: testurl + ID + "/accept", method: "POST", headers: header }).then((res) => {
+              const { body } = res;
+              let jsonBody = JSON.parse(body);
+              $notify(jsonBody.data.name, "đã tham gia thành công", "");
+              console.log(jsonBody.data.name + " đã tham gia thành công");
+/*               ids = $prefs.valueForKey("APP_ID").split(",");
+              // ids = ids.filter((ids) => ids !== ID);
+              $prefs.setValueForKey(ids.toString(), "APP_ID"); */
+              resolve();
+            });
+          }
+        }
+      },
+      (error) => {
+        if (error == "The request timed out.") {
+          resolve();
+        } else {
+          $notify("Tự động tham gia TF: ", error, "");
+          console.log(ID + " " + error);
+          resolve();
+        }
+      }
+    );
+  });
+}
